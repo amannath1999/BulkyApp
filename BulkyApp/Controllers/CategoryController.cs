b318510a8a93e216d5f6b214403c6db36f10f610
Aman Nath <amannath1709@outlook.com>
2025-04-25 00:13:09 +0530

    check in for day 3

---
+++ BulkyApp/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using BulkyApp.Data;
+using BulkyApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
