f783b97758f54629b738fb71c0400ca488ba8cc5
Aman Nath <amannath1709@outlook.com>
2025-04-27 00:35:56 +0530

    day 5 of learning: server side and client side validation + asp-validation-for and asp-validation-summary

--- BulkyApp/Controllers/CategoryController.cs
+++ BulkyApp/Controllers/CategoryController.cs
@@ -20,5 +20,52 @@ namespace BulkyApp.Controllers
         {
             return View();
         }
+
+
+        //public IActionResult InsertIntoDB(Category obj)
+        //{
+        //    _db.Categories.Add(obj);
+        //    _db.SaveChanges();
+        //    //return View("Index");
+        //    return RedirectToAction("Index");
+        //}
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "Name cannot be exactly same as Display Order");
+            }
+
+            if (obj.Name!= null && obj.Name.ToLower() == "test")
+            {
+                ModelState.AddModelError("", "Test is not a valid category name");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                //return View("Index");
+                return RedirectToAction("Index");
+            }
+            return View();
+            
+        }
+
+        //[HttpPost]
+        //public IActionResult InsertIntoDB(Category obj)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Categories.Add(obj);
+        //        _db.SaveChanges();
+        //        //return View("Index");
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+
+        //}
     }
 }
